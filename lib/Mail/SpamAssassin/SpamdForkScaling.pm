645e5da16420337d6768688471fa5635a1b18c98
J. Nick Koston <nick@cpanel.net>
2016-01-02 15:17:47 -0600

    wip

--- lib/Mail/SpamAssassin/SpamdForkScaling.pm
+++ lib/Mail/SpamAssassin/SpamdForkScaling.pm
@@ -403,24 +403,29 @@ sub consider_going_dormant {
     if ( ++$self->{number_of_dormant_loops} == $NUMBER_OF_LOOPS_TO_GO_DORMANT ) {
         my %current_flags;
         my @cmdline;
+my @listen_args;
         {
             #go dormant
             # Remove CLOEXEC
-            foreach my $fileno ( keys %{ $self->{backchannel}->{fileno_to_fh} } ) {
-                my $fh = $self->{backchannel}->{fileno_to_fh}->{$fileno};
+            foreach (my $i=0;$i<=$#{$self->{server_fh}};$i++) {
+                my $fileno = $self->{server_fileno}->[$i];
+                my $fh = $self->{server_fh}->[$i];
                 $current_flags{$fileno} = fcntl( $fh, Fcntl::F_GETFD(), 0 ) or warn "Failed to get flags on fileno: $fileno";
                 my $new_flags = $current_flags{$fileno} & ~&Fcntl::FD_CLOEXEC;
                 fcntl( $fh, Fcntl::F_SETFD(), $new_flags ) or warn "Failed to set flags on fileno: $fileno";
+                push @listen_args, $fileno . '=' . scalar ref $fh;
             }
-            @cmdline = ( '/usr/local/cpanel/libexec/spamd-dormant', '--listen=' . join( ',', map { "$_=" . ref $self->{backchannel}->{fileno_to_fh}->{$_}  } sort keys %{ $self->{backchannel}->{fileno_to_fh} } ));    # Do not add or die as we want to continue running if this fails
+            @cmdline = ( '/usr/local/cpanel/libexec/spamd-dormant', '--listen=' . join( ',', @listen_args ));    # Do not add or die as we want to continue running if this fails
             exec(@cmdline);
         }
 
 
         warn "prefork: failed to exec @cmdline: $!";
         # Restore flags
-        foreach my $fileno ( keys %{ $self->{backchannel}->{fileno_to_fh} } ) {
-            my $fh = $self->{backchannel}->{fileno_to_fh}->{$fileno};
+      foreach (my $i=0;$i<=$#{$self->{server_fh}};$i++) {
+                my $fileno = $self->{server_fileno}->[$i];
+                my $fh = $self->{server_fh}->[$i];
+       
             fcntl( $fh, Fcntl::F_SETFD(), $current_flags{$fileno} ) or warn "Failed to restore flags on fileno: $fileno";
 
         }
